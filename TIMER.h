cd3ba2e38913c09913cd66b881f07c79b0f85a6f
Talash Ganvir <talash.ganvir@ppp.net>
2019-12-01 16:54:15 +0530

    my timer header

---
+++ TIMER.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   TIMER.h
+ * Author: talash.ganvir
+ *
+ * Created on 21 September, 2019, 10:11 AM
+ */
+
+#ifndef TIMER_H
+#define	TIMER_H
+
+#include <xc.h>
+
+void TOdelay_Ms(float COUNT);
+#endif	/* TIMER_H */
+
