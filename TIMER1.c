f76d912fb55ebbefa59ab32d18304ce2cda196c8
Talash Ganvir <talash.ganvir@ppp.net>
2019-12-03 11:32:43 +0530

    sixth version of code

--- TIMER1.c
+++ TIMER1.c
@@ -1,15 +1,17 @@
 #include"TIMER1.h"
 
 void timer_init1(void) {
+
+
     T1CONbits.T1CKPS1 = 1; //PRRESCALE 1:4
     T1CONbits.T1CKPS0 = 1;
 
     T1CONbits.RD16 = 1; //TIMER1 16BIT MODE
 
     T1CONbits.TMR1CS0 = 0; //Timer1 clock source is the instruction clock (FOSC/4)
-    T1CONbits.TMR1CS1 = 0;
+    T1CONbits.TMR1CS1 = 0;// fakt test kartoy
 
-    T1CONbits.TMR1ON = 1; //TIMER1 ON dshiaihkjahiub uashckjn
+    T1CONbits.TMR1ON = 1; //TIMER1 ON
 
     TMR1 = 0xFFFF - TIMER1_SEC_COUNT;
 }
